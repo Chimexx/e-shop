d978c860ba38e634e0b50eeb7aaf41226800eb75
Chimexx <mezie.cjo@gmail.com>
2021-11-07 14:18:49 +0100

    continued to build on redux(persisting login state)

--- src/pages/Login/Login.styles.jsx
+++ src/pages/Login/Login.styles.jsx
@@ -1,85 +1,99 @@
-import styled from 'styled-components'
-import bg from '../../assets/bg.jpg'
-import { mobile } from '../../Responsive'
+import styled from "styled-components";
+import bg from "../../assets/bg.jpg";
+import { mobile } from "../../Responsive";
 
 export const Container = styled.div`
-	width: 100vw;
-	height: 100vh;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	background: linear-gradient(
-			rgba(255, 255, 255, 0.5),
-			rgba(255, 255, 255, 0.9)
-		),
-		url(${bg});
-`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.9)
+    ),
+    url(${bg});
+`;
 export const Wrapper = styled.div`
-	padding: 20px;
-	width: 40%;
-	background-color: rgba(255, 255, 255, 0.8);
-	border-radius: 20px;
+  padding: 20px;
+  width: 40%;
+  background-color: rgba(255, 255, 255, 0.8);
+  border-radius: 20px;
 
-	${mobile({ padding: '10px', width: '90%' })}
-`
+  ${mobile({ padding: "10px", width: "90%" })}
+`;
 export const Form = styled.div`
-	display: flex;
-	flex-wrap: wrap;
+  display: flex;
+  flex-direction: column;
+  /* flex-wrap: wrap; */
 
-	${mobile({ flexWrap: 'no-wrap', padding: '10px' })}
-`
+  ${mobile({ flexWrap: "no-wrap", padding: "10px" })}
+`;
 export const Title = styled.h1`
-	font-size: 24px;
-	font-weight: 300;
+  font-size: 24px;
+  font-weight: 300;
 
-	${mobile({ padding: '10px' })}
-`
+  ${mobile({ padding: "10px" })}
+`;
 export const Input = styled.input`
-	flex: 1;
-	min-width: 40%;
-	margin: 20px 10px 0 0;
-	padding: 7px;
-	font-size: 16px;
-	font-weight: 500;
-	outline: none;
-	border: none;
-	border-radius: 5px;
-	color: #05021a;
+  flex: 1;
+  min-width: 40%;
+  margin: 20px 10px 0 0;
+  padding: 7px;
+  font-size: 16px;
+  font-weight: 500;
+  outline: none;
+  border: none;
+  border-radius: 5px;
+  color: #05021a;
 
-	${mobile({ display: 'block', flex: 'none', minWidth: '95%' })}
-`
+  ${mobile({ display: "block", flex: "none", minWidth: "95%" })}
+`;
 
 export const Button = styled.button`
-	margin-top: 20px;
-	width: 50%;
-	padding: 5px;
-	border: 1px solid orangered;
-	border-radius: 5px;
-	background-color: orangered;
-	color: white;
-	transition: ease all 0.3s;
+  margin-top: 20px;
+  width: 50%;
+  padding: 5px;
+  border: 1px solid orangered;
+  border-radius: 5px;
+  background-color: orangered;
+  color: white;
+  transition: ease all 0.3s;
 
-	${mobile({ width: '100%' })}
+  &:disabled {
+    background-color: #f7c7b5;
+    color: #575757;
+    cursor: not-allowed;
+    border: 1px solid #f7c7b5;
+  }
 
-	&:hover {
-		color: orangered;
-		background-color: white;
-	}
-`
+  ${mobile({ width: "100%" })}
+
+  &:hover {
+    color: orangered;
+    background-color: white;
+  }
+`;
 export const Signup = styled.p`
-	margin-top: 10px;
-	font-size: 14px;
-`
+  margin-top: 10px;
+  font-size: 14px;
+`;
 export const Footer = styled.div`
-	${mobile({ padding: '10px' })}
-`
+  ${mobile({ padding: "10px" })}
+`;
 
 export const ForgotPassword = styled.p`
-	margin-top: 5px;
-	font-size: 14px;
-`
+  margin-top: 5px;
+  font-size: 14px;
+`;
 export const Link = styled.a`
-	font-weight: 500;
-	color: orangered;
-	cursor: pointer;
-`
+  font-weight: 500;
+  color: orangered;
+  cursor: pointer;
+`;
+
+export const Error = styled.span`
+  color: red;
+  font-size: 14px;
+  margin-top: 10px;
+`;
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,42 +1,68 @@
-import { Divider } from '@mui/material'
-import React from 'react'
+import { Divider } from "@mui/material";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../../redux/apiCalls";
 
 //styles
 import {
-	Container,
-	Wrapper,
-	Input,
-	Title,
-	Form,
-	Signup,
-	Button,
-	ForgotPassword,
-	Link,
-	Footer,
-} from './Login.styles'
+  Container,
+  Wrapper,
+  Input,
+  Title,
+  Form,
+  Signup,
+  Button,
+  ForgotPassword,
+  Link,
+  Footer,
+  Error,
+} from "./Login.styles";
 
 const Login = () => {
-	return (
-		<Container>
-			<Wrapper>
-				<Title>SIGN IN</Title>
-				<Form>
-					<Input placeholder='Username' />
-					<Input placeholder='Password' />
-					<Button>SIGN IN</Button>
-				</Form>
-				<Footer>
-					<Divider style={{ marginTop: 20 }} />
-					<Signup>
-						Don't have an account? <Link>Register</Link>
-					</Signup>
-					<ForgotPassword>
-						Forgot your password? <Link>Reset</Link>
-					</ForgotPassword>
-				</Footer>
-			</Wrapper>
-		</Container>
-	)
-}
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Login
+  const { isFetching, error } = useSelector((state) => state.user);
+
+  const dispatch = useDispatch();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    login(dispatch, { username, password });
+  };
+
+  return (
+    <Container>
+      <Wrapper>
+        <Title>SIGN IN</Title>
+        <Form>
+          <Input
+            type="text"
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Username"
+          />
+          <Input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+          <Button disabled={isFetching} onClick={handleLogin}>
+            SIGN IN
+          </Button>
+          {error && <Error>Something went wrong, please try again.</Error>}
+        </Form>
+        <Footer>
+          <Divider style={{ marginTop: 20 }} />
+          <Signup>
+            Don't have an account? <Link>Register</Link>
+          </Signup>
+          <ForgotPassword>
+            Forgot your password? <Link>Reset</Link>
+          </ForgotPassword>
+        </Footer>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Login;
